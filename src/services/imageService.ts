4c68a727364770f6fb26cf1194060d3fa8082a4b
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-10-23 11:19:46 -0500

    fix imageService

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,34 +1,33 @@
+import { data } from '../data'; // Ajusta la ruta según donde esté ubicado el archivo
+
 const isProduction = process.env.NODE_ENV === 'production';
 
-
 export const fetchImages = async (page: number) => {
-  let response;
-
   if (isProduction) {
-    // En producción, usa el archivo JSON estático
-    console.log('estoy en produccion....actulizado',isProduction);
-    response = await fetch(`/data.json`);
-    console.log('response images....',response);
+    // En producción, usa el archivo estático importado desde data.ts
+    console.log('Estoy en producción....', isProduction);
+    console.log('actulizado el archivo  ****');
+    return data; // Retorna directamente los datos desde el archivo
   } else {
-    console.log('estoy en desarrollo');
+    console.log('Estoy en desarrollo');
     // En desarrollo, usa la API mock
-    response = await fetch(`http://localhost:3100/images?page=${page}`);
-  }
+    const response = await fetch(`http://localhost:3100/images?page=${page}`);
 
-  if (!response.ok) {
-    throw new Error('Error al obtener imágenes');
+    if (!response.ok) {
+      throw new Error('Error al obtener imágenes');
+    }
+    return response.json();
   }
-  return response.json();
 };
 
 export const toggleLike = async (id: number) => {
   if (isProduction) {
-    // En producción, no hay API para manejar likes, por lo que simularemos la respuesta
+    // En producción, simulamos el toggle de like
     console.warn(`Simulando el toggle de like para la imagen con ID ${id} en producción.`);
-    return { status: 204 }; // Simular un status de éxito
+    return { status: 204 }; // Simulamos un status de éxito
   } else {
-     console.log('like estoy en desarrollo ');
-    // En desarrollo, usa la API mock
+    console.log('Like en desarrollo');
+    // En desarrollo, usamos la API mock
     const response = await fetch(`http://localhost:3100/images/${id}/likes`, {
       method: 'POST',
     });
